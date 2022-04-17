9bc4c2fa4566a3f03fdc55a3f357c77cea785775
Elmosh <61812199+Elmosh32@users.noreply.github.com>
2022-04-17 15:14:25 +0300

    Update Cell.js

--- js/Cell.js
+++ js/Cell.js
@@ -1,5 +1,5 @@
 class Cell {
-  
+
   constructor(val) {
     this.val = val;
     this.isDraft = false;
@@ -8,39 +8,39 @@ class Cell {
 
 
   render() {
-      let cellDiv = document.createElement("div");
-      let cellNum = document.createElement("div");
-      let cellTable;
-      let tblStr;
-      cellDiv.setAttribute("class", "cell");
-      cellNum.setAttribute("class", "cell-number");
-      if(this.isFixed == true)
-          cellNum.classList.add("cell-fixed");
+    let cellDiv = document.createElement("div");
+    let cellNum = document.createElement("div");
+    let cellTable;
+    let tblStr;
+    cellDiv.setAttribute("class", "cell");
+    cellNum.setAttribute("class", "cell-number");
+    if (this.isFixed == true)
+      cellNum.classList.add("cell-fixed");
 
-      cellNum.innerHTML = this.val;
+    cellNum.innerHTML = this.val;
 
-      cellTable = document.createElement("table");
-      tblStr = this.createTable();
-      cellTable.setAttribute("class", "cell-table");
-      cellTable.innerHTML = tblStr;
+    cellTable = document.createElement("table");
+    tblStr = this.createTable();
+    cellTable.setAttribute("class", "cell-table");
+    cellTable.innerHTML = tblStr;
 
-      cellDiv.append(cellNum);
-      cellDiv.append(cellTable);
-      return cellDiv;
+    cellDiv.append(cellNum);
+    cellDiv.append(cellTable);
+    return cellDiv;
   }
 
   createTable() {
-      let str = "";
+    let str = "";
 
-      for(let i = 1; i <= 3; i++) {
-          str += "<tr>";
-          for(let j = 1; j <= 3; j++) {
-              str += "<td></td>";
-          }
-          str += "</tr>";
+    for (let i = 1; i <= 3; i++) {
+      str += "<tr>";
+      for (let j = 1; j <= 3; j++) {
+        str += "<td></td>";
       }
+      str += "</tr>";
+    }
 
-      return str;
+    return str;
   }
 
 }
